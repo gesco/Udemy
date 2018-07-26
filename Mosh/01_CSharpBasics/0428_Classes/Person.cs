864ff357962b09bb16fbaa909ac7995247b557a8
gesco <gescobedoz@gmail.com>
2018-07-25 23:47:38 -0500

    Demo on Classes

---
+++ Mosh/01_CSharpBasics/0428_Classes/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _0428_Classes
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+
+    }
+}
---
+++ Mosh/01_CSharpBasics/0428_Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using _0428_Classes.Math;
+using System;
+
+
+namespace _0428_Classes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person john = new Person();
+            var john = new Person();
+
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
