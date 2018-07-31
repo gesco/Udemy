55a902487b7b8688936b567eed0cde98104bf783
gesco <gescobedoz@gmail.com>
2018-07-30 22:58:37 -0500

    How to declare and initialize an array in C#

---
+++ Mosh/01_CSharpBasics/0431_Arrays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0431_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] number = new int[3];
+            //          or
+            var numbers = new int[3];
+
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);  // 1
+            Console.WriteLine(numbers[1]);  // 0
+            Console.WriteLine(numbers[2]);  // 0
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);    // True
+            Console.WriteLine(flags[1]);    // False
+            Console.WriteLine(flags[2]);    // False
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+
+            Console.WriteLine(names[0]);    // Jack
+        }
+    }
+}
