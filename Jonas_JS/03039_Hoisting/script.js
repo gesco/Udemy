17448fc770c14cfe55f8d11ddd7432b4ed2cb0ee
gesco <gescobedoz@gmail.com>
2019-01-09 23:53:13 -0600

    Hoisting in Practice

--- Jonas_JS/03039_Hoisting/script.js
+++ Jonas_JS/03039_Hoisting/script.js
@@ -1,18 +1,32 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
-calculateAge(1990);
+// functions
+calculateAge(1965);
 
 function calculateAge(year) {
-  console.log(2019 - year);
+  console.log(2016 - year);
 }
 
+
+// retirement(1956);
 var retirement = function(year) {
-  console.log(65 - (2019 - year));
+  console.log(65 - (2016 - year));
 };
 
-retirement(1990);
+// variables
 
+
+var age = 23;
+console.log(age);
+
+function foo() {
+  console.log(age);
+  var age = 65;
+  console.log(age);
+}
+foo();
+console.log(age);
 
 
 
