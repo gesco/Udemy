23e2bcdb30fef07f63168ebd2d92e4540aaee4bf
gesco <gescobedoz@gmail.com>
2019-01-15 23:46:56 -0600

    Scoping - removed code

--- Jonas_JS/03040_Scoping/script.js
+++ Jonas_JS/03040_Scoping/script.js
@@ -1,47 +1,4 @@
 ///////////////////////////////////////
-// Lecture: Hoisting
-
-// functions
-calculateAge(1965);
-
-function calculateAge(year) {
-  console.log(2016 - year);
-}
-
-
-// retirement(1956);
-var retirement = function(year) {
-  console.log(65 - (2016 - year));
-};
-
-// variables
-
-
-var age = 23;
-console.log(age);
-
-function foo() {
-  console.log(age);
-  var age = 65;
-  console.log(age);
-}
-foo();
-console.log(age);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////
 // Lecture: Scoping
 
 
