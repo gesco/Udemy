bc3a898a9a23c8f2a7ad45ba92e66d69ec6fabdb
gesco <gescobedoz@gmail.com>
2019-01-25 01:05:24 -0600

    Events and Event Handling: Rolling the Dice

--- Jonas_JS/04047_DOMProject/app.js
+++ Jonas_JS/04047_DOMProject/app.js
@@ -15,15 +15,35 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-// Setter
-document.querySelector('#current-' + activePlayer).textContent = dice;
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+//console.log(dice);
 
-// Getter
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+
+//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. Random number
+  dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM =   document.querySelector('.dice')
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+});
+
+// Setter
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// Getter
+// var x = document.querySelector('#score-0').textContent;
