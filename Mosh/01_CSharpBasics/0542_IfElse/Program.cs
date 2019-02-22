64648698ab0d481bce68116eed762aee44d34e27
gesco <gescobedoz@gmail.com>
2019-02-22 05:26:03 -0600

    If/Else and Switch/Case

---
+++ Mosh/01_CSharpBasics/0542_IfElse/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0542_190221_IfElseSwitchCase
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+                Console.WriteLine("It's morning.");
+            else if (hour >= 12 && hour < 18)
+                Console.WriteLine("It's afternoon.");
+            else
+                Console.WriteLine("It's evening");
+
+            bool isGoldCustomer = true;
+            //if (isGoldCustomer)
+            //    price = 19.95f;
+            //else
+            //    price = 29.95f;
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            Console.WriteLine(price);
+
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Spring:
+                    break;
+                case Season.Summer:                    
+                case Season.Autumn:
+                    Console.WriteLine("We've got promotions");
+                    break;
+                case Season.Winter:
+                    break;
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ Mosh/01_CSharpBasics/0542_IfElse/Season.cs
@@ -0,0 +1,10 @@
+﻿namespace _0542_190221_IfElseSwitchCase
+{
+    public enum Season
+    {
+        Spring,
+        Summer, 
+        Autumn,
+        Winter
+    }
+}
