fa6fe2edb730bba34d517df0679bf913a1cd3bc4
gesco <gescobedoz@gmail.com>
2019-02-22 05:18:22 -0600

    Reference Types and Value Types

---
+++ Mosh/01_CSharpBasics/0437_ReferenceType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0437_RefTypeValueType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b)); 
+            // a: 10, b: 11
+
+            // An array is a class, a refrence type
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+            // array1[0]: 0, array2[0]: 0
+        }
+    }
+}
