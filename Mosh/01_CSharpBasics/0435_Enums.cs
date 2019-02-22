96661e8f924decb33f72491b405b68611f0b7366
gesco <gescobedoz@gmail.com>
2019-02-21 23:48:07 -0600

    Enums

---
+++ Mosh/01_CSharpBasics/0435_Enums.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0435_Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    public enum CustomerType
+    {
+        Bronze,
+        Silver,
+        Gold
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method); // Returns 3
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId); // Returns Express
+
+            Console.WriteLine(method.ToString()); // Returns Express
+            Console.WriteLine(method); // Returns Express
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+
+            var type = (int)CustomerType.Bronze;
+            Console.WriteLine(type); // Returns 0
+        }
+    }
+}
