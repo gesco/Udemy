d3e67c1ef7a1b53b76ed7bbc4c18d8b5506b51f4
gesco <gescobedoz@gmail.com>
2019-02-26 23:58:26 -0600

    added search.ejs

--- Steele_MEAN/027_283_MovieSearchApp/app.js
+++ Steele_MEAN/027_283_MovieSearchApp/app.js
@@ -3,9 +3,15 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+  res.render("search");
+});
+
 app.get("/results", function(req, res){
   //res.send("Hello, it works!");
-  request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+  var query = req.query.search;
+  var url = "http://omdbapi.com/?s" + query + "=iowa&apikey=thewdb";
+  request(url, function(error, response, body){
     if(!error && response.statusCode == 200){
       //res.send(body);
       var data = JSON.parse(body);
